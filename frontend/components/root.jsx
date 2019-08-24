d2f2ba6792ea619a116efaefffb05a3cd3cffc7a
Bryan Justo <tsarbelyyvolk@gmail.com>
2019-08-24 01:56:28 -0400

    init commit

---
+++ frontend/components/root.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// When ever the document has been loaded it executes everything inside the call back
+const Root = () => {
+  return (
+    <table className="tables">
+      <tr>
+        <th> Name</th>
+      </tr>
+      <tr>
+        <td> John</td>
+      </tr>
+      <tr>
+        <td> Elsa</td>
+      </tr>
+      </table>
+  )
+}
+export default Root;
---
+++ frontend/entry.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/root';
+
+// When ever the document has been loaded it executes everything inside the call back
+document.addEventListener('DOMContentLoaded', () =>
+{
+  let x = 1;
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root />, root);
+});
